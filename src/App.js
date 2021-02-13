656eadf037360c54af54f5812d28875c218c0a1d
GtzRicky <ricardogusar93@hotmail.com>
2021-02-13 01:06:19 -0600

    add netlify folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Data from './quotes.json';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import randomColor from 'randomcolor';
+
+
+const randomArray = (length) => Math.floor(Math.random() * length);
+
+const getQuote = () => Data.quotes[randomArray(Data.quotes.length)];
+
+const QuoteBox = ({ quoteShowed, colorShowed, getNewQuote }) => {
+
+  const { quote, author } = quoteShowed;
+
+  return (
+    <div className="QuoteBox">
+      <div className="TextQuote" style={{color: colorShowed}}>"{quote}"</div>
+      <div className="Author" style={{color: colorShowed}}>~ {author}</div>
+      <div className="Links">
+        <a href="https://twitter.com/intent/tweet" className="Icon">
+          <FontAwesomeIcon icon={faTwitter} size="2x"/>
+        </a>
+        <button className="Button" style={{color: colorShowed}} onClick={getNewQuote}>New Quote</button>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+
+  const [color, setColor] = useState(randomColor());
+  const [newQuote, setNewQuote] = useState(getQuote());
+
+  const handleQuoteChange = () => {
+    setNewQuote(getQuote());
+    setColor(randomColor());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: color}}>
+      <QuoteBox quoteShowed={newQuote} colorShowed={color} getNewQuote={handleQuoteChange}/>
     </div>
   );
 }
